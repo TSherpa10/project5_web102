3a0a9fba19467a3836b2a324f2a1d5bc72fbca58
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2023-10-18 17:12:39 -0400

    changed emote

--- data-dashboard/src/components/Sidebar.jsx
+++ data-dashboard/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 const SideBar = () => {
     return (
         <div className="sidebar-div">
-            <h1 className="title-text">The Cat Logs 😸📒</h1>
+            <h1 className="title-text">The Cat Logs 😸📚</h1>
             <div className="sidebar-options">
                 <button className="btn-block"><h2>🎯 Dashboard</h2></button>
                 <button className="btn-block"><h2>🔍 Search</h2></button>
